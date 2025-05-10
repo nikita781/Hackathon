d36c42b997d26b6a7062482f4c5ac39a0ec971d8
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2025-05-10 21:32:14 +0400

    add image (MorphTo)
    Took 21 seconds

---
+++ app/Models/Image.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Image extends Model
+{
+    protected $fillable = [
+        'imageable_id', 'imageable_type', 'path', 'title', 'mime', 'order',
+    ];
+
+    public function imageable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
