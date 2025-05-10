70916bcf1917eb3986a5abc664c572b878069cf7
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2025-05-10 23:41:32 +0400

    add File
    Took 28 minutes

---
+++ app/Models/File.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class File extends Model
+{
+    protected $fillable = [
+        'fileable_id', 'fileable_type', 'path', 'title', 'mime', 'order',
+    ];
+
+    public function fileable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
