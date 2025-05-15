f0b05599a0f118a5ae9e26979832656a9f9e35cb
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2025-05-15 17:10:27 +0400

    add TabResource
    Took 11 seconds

---
+++ app/Http/Resources/TabResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use PHPUnit\TextUI\Configuration\FileCollection;
+
+/** @mixin \App\Models\Tab */
+class TabResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'images' => ImageResource::collection($this->whenLoaded('images')),
+            'files' => FileResource::collection($this->whenLoaded('files')),
+        ];
+    }
+}
