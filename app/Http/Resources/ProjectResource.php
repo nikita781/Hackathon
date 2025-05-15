5d069e728b50481592da91604024d61362cee903
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2025-05-15 17:10:15 +0400

    add ProjectResource
    Took 13 seconds

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+/** @mixin \App\Models\Project */
+class ProjectResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'preview_path' => $this->preview_path ? Storage::url($this->preview_path) : null,
+            'about' => $this->about,
+            'stack' => $this->stack,
+            'project_link' => $this->project_link,
+            'presentation_path' => $this->presentation_path ? Storage::url($this->presentation_path) : null,
+            'video_link' => $this->video_link,
+            'is_published' => $this->is_published,
+            'capitan' => new UserResource($this->whenLoaded('capitan')),
+            'members' => UserResource::collection($this->whenLoaded('members')),
+            'images' => ImageResource::collection($this->whenLoaded('images')),
+
+            'hackathon' => new HackathonResource($this->whenLoaded('hackathon')),
+        ];
+    }
+}
