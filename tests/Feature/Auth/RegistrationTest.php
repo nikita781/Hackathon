a2edb3e4351034d3c33c7e01e06375f61179010c
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2025-06-17 15:30:34 +0400

    update test
    Took 33 minutes

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -21,9 +21,9 @@ class RegistrationTest extends TestCase
     {
         $response = $this->post('/register', [
             'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
-            'password_confirmation' => 'password',
+            'email' => 'example@example.com',
+            'password' => '123qwe123qwe',
+            'password_confirmation' => '123qwe123qwe',
         ]);
 
         $this->assertAuthenticated();
