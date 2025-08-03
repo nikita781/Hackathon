907d404a21be6decf7512cf2d4594f4f4d77182c
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2025-08-03 22:47:01 +0400

    fix: Небольшие улучшения
    Took 4 minutes

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -29,7 +29,7 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        $user = $request->user()->load('roles');
+        $user = $request->user()?->load('roles');
         return array_merge(parent::share($request), [
             'flash' => function () use ($request) {
                 return [
@@ -39,7 +39,7 @@ class HandleInertiaRequests extends Middleware
             },
             'auth' => [
                 'user' => $user,
-                'roles' => $user->roles->pluck('title'),
+                'roles' => $user?->roles?->pluck('title'),
             ],
             'notifications' => [
                 'unread' => $request->user()?->unreadNotifications()->exists(),
