c93658944a9935467ef25a01fd435f7c9285dc5c
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2025-09-22 16:35:17 +0400

    fix: Небольшие улучшения
    Took 4 minutes

--- app/Http/Requests/TabRequest.php
+++ app/Http/Requests/TabRequest.php
@@ -28,12 +28,12 @@ class TabRequest extends FormRequest
             'sections.*.items.*.id' => ['nullable', 'integer', 'exists:tab_items,id'],
             'sections.*.items.*.title' => ['required_with:sections.*.items', 'string', 'max:255'],
             'sections.*.items.*.content' => ['nullable', 'string'],
-            'sections.*.items.*.image_path' => ['nullable', 'image', 'mimes:jpeg,png,jpg,webp', 'max:2048'],
+            'sections.*.items.*.image_path' => ['nullable', 'image', 'mimes:jpeg,png,jpg,webp', 'max:5048'],
 
             'partners' => ['nullable', 'array'],
             'partners.*' => ['image', 'mimes:jpeg,png,jpg,webp', 'max:2048'],
             'files' => ['nullable', 'array'],
-            'files.*' => ['file', 'max:2048'],
+            'files.*' => ['file', 'max:5048'],
             'delete_media_ids' => ['nullable', 'array'],
             'delete_media_ids.*' => ['integer', 'exists:media,id'],
         ];
