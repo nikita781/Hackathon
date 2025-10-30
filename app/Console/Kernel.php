2145088bac6b7f2fd55c84990cd903d76304b6d2
Asparagusssa <120052922+Asparagusssa@users.noreply.github.com>
2025-10-30 20:17:22 +0400

    fix: Вызов команды завершения хакатона
    Took 9 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('hackathons:finish')->hourlyAt('10');
+        $schedule->command('hackathons:finish')->everyMinute();
         $schedule->command('users:sync')->dailyAt('03:00');
         $schedule->command('editor:cleanup')->weeklyOn(6, '04:00');
     }
